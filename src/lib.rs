878f34e1c995de7795ce6d567e055e69f05f1992
Daniel Haarhoff <daniel@rknt.de>
2023-04-23 09:03:42 +0100

    Update model from textarea input

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,41 @@
-// (Lines like the one below ignore selected Clippy rules
-//  - it's useful when you want to check your code with `cargo make verify`
-// but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
 
 use seed::{prelude::*, *};
 
-// ------ ------
-//     Init
-// ------ ------
-
-// `init` describes what should happen when your app started.
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model { counter: 0 }
-}
-
-// ------ ------
-//     Model
-// ------ ------
-
-// `Model` describes our app state.
-struct Model {
-    counter: i32,
-}
-
-// ------ ------
-//    Update
-// ------ ------
-
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
-// `Msg` describes the different events you can modify state with.
-enum Msg {
-    Increment,
-}
-
-// `update` describes how to handle each `Msg`.
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
-    match msg {
-        Msg::Increment => model.counter += 1,
+    Model {
+        army: ("".to_string()),
     }
 }
 
-// ------ ------
-//     View
-// ------ ------
+struct Model {
+    army: String,
+}
 
-// `view` describes what to display.
+#[derive(Clone)]
+enum Msg {
+    ArmyUpdated(String),
+}
+
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::ArmyUpdated(input) => model.army = input,
+    }
+}
+
 fn view(model: &Model) -> Node<Msg> {
     div![
-        textarea!(
+        textarea![
             C!["paste-area"],
-            attrs!{At::Rows => 10}
-        )
+            attrs! {At::Rows => 10},
+            input_ev(Ev::Change, Msg::ArmyUpdated),
+            input_ev(Ev::KeyUp, Msg::ArmyUpdated)
+        ],
+        p!(model.army.clone())
     ]
 }
 
-// ------ ------
-//     Start
-// ------ ------
-
-// (This function is invoked by `init` function in `index.html`.)
 #[wasm_bindgen(start)]
 pub fn start() {
-    // Mount the `app` to the element with the `id` "app".
     App::start("app", init, update, view);
 }
